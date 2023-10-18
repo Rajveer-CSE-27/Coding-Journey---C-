e1b2913652d8804d9c8f9a48278bc7c6b17b809e
Rajveersinh Gohil <140502093+Rajveer-CSE-27@users.noreply.github.com>
2023-10-18 06:13:56 +0530

    Update recursionSolution.cpp

--- 2 Dynamic Programming/13 3-d DP : Ninja and his friends/recursionSolution.cpp
+++ 2 Dynamic Programming/13 3-d DP : Ninja and his friends/recursionSolution.cpp
@@ -56,7 +56,6 @@ int main() {
 	int n, m;
 	cin >> n >> m;
 	vector<vector<int>> grid(n, vector<int>(m));
-	vector<int> dp(m);
 	for(int i=0; i<n; i++) {
 		for(int j=0; j<m; j++) {
 			cin >> grid[i][j];
