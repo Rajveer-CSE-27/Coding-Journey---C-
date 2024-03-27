be0dddbc41eb5706839de27e070226945fb6b334
Rajveersinh Gohil <140502093+Rajveer-CSE-27@users.noreply.github.com>
2024-03-27 12:02:27 +0530

    Create intro.cpp

---
+++ Number Theory CodeNCode/Non Deterministic Primality Tests/intro.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+/**
+
+Deterministic Primality Test
+	A primality test which tests a number n is prime or not with 100% accuracy.
+
+Non-Deterministic Primality Test
+	A primality test which tests a number n is prime or not with a small error (like fermat's primality test)
+
+Where needed ? 
+	Questions like : Given Q(Q <=10^5) queries, in each query you are given N (N<=10^12) , print whether
+					N is prime or not.
+					
+					Here total time will be greater than 10^9 hence TLE
+					Or if N <= 2^63 - 1 and Q <= 500 then also TLE :: SPOJ Problem
+
+
+Fermat Primality Test : 
+	The simplest Non-Deterministic Primality test technique which is based on Fermat's Little Theorem.
+	
+Miller-Rabin Primality Test :
+	An improvised version Fermat primality test, it is also based on Fermat Little Theorem.
+	
+Deterministic version of Miller-Rabin Primality Test :
+	We will study how Miller-Rabin test can also be used to test primes with 100% accuracy for 32 and 64 bit integers.
+					
+
+**/
+
+
+
+
+int main() {
+	
+	
+	return 0;
+}
