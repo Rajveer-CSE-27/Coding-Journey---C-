aff096d14017c425c858fcc3e686a10958fbc70b
Rajveersinh Gohil <140502093+Rajveer-CSE-27@users.noreply.github.com>
2024-06-13 19:09:35 +0530

    Update 416. Partition Equal Subset Sum.cpp

--- Dynamic Programming/Karthik Arora/LeetCode DP Patterns/416. Partition Equal Subset Sum.cpp
+++ Dynamic Programming/Karthik Arora/LeetCode DP Patterns/416. Partition Equal Subset Sum.cpp
@@ -1,38 +1,5 @@
 class Solution {
 public:
-
-    vector<vector<char>> dp;
-
-    bool solve(int index, int sm, vector<int>& nums, int& n) {
-        if(index == n) {
-            return false;
-        }
-        if(sm == 0) {
-            return true;
-        }
-        if(dp[index][sm] != '2') {
-            if(dp[index][sm] == '1') {
-                return true;
-            }
-            return false;
-        }
-        bool take = false, notTake = false;
-        if(sm >= nums[index]) {
-            take = solve(index+1, sm - nums[index], nums, n);
-        }
-        notTake = solve(index+1, sm, nums, n);
-
-        bool ans = take or notTake;
-        if(ans == true) {
-            dp[index][sm] = '1';
-        } else {
-            dp[index][sm] = '0';
-        }
-        
-        return ans;
-
-    }
-
     bool canPartition(vector<int>& nums) {
         int sm = 0;
         int n = nums.size();
@@ -44,8 +11,29 @@ public:
             return false;
         }
         sm /= 2;
-        dp.resize(n, vector<char>(sm + 1, '2'));
+        int dp[n][sm+1];
 
-        return solve(0,sm,nums,n);
+        for(int i=0; i<n; i++) {
+            dp[i][0] = true;
+        }
+        for(int i=0; i<n; i++) {
+            for(int j=1; j<=sm; j++) {
+                if(i == 0) {
+                    if(nums[i] == j) {
+                        dp[i][j] = true;
+                    } else {
+                        dp[i][j] = false;
+                    }
+                } else {
+                    bool take = false;
+                    if(j >= nums[i]) {
+                        take = dp[i-1][j - nums[i]];
+                    }
+                    bool notTake = dp[i-1][j];
+                    dp[i][j] = take or notTake;
+                }
+            }
+        }
+        return dp[n-1][sm];
     }
 };
