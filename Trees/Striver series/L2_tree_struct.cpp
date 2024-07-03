8a7beda8e6d1d66b110cd241e910eade55146e6f
Rajveersinh Gohil <140502093+Rajveer-CSE-27@users.noreply.github.com>
2024-07-03 06:55:49 +0530

    Create L2_tree_struct.cpp

---
+++ Trees/Striver series/L2_tree_struct.cpp
@@ -0,0 +1,33 @@
+// || जय जय जय बजरंग बली ||
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+/**
+https://www.youtube.com/watch?v=ctCpP0RFDFc&list=PLkjdNRgDmcc0Pom5erUBU4ZayeU9AyRRu&index=2
+**/
+
+struct node {
+    
+    int data;
+    struct node *left;
+    struct node *right;
+    
+    node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+int main() {
+    
+    struct node *root = new node(1);
+    root->left = new node(0);
+    root->right = new node(2);
+    
+    root->right->right = new node(3);
+    
+    cout << root->left->data << " " << root->data << " " << root->right->data << " " << root->right->right->data;
+    
+    return 0;
+}
