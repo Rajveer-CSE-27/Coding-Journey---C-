e20c26ec75d856d06d6d24f35234fd2c1406d682
Rajveersinh Gohil <140502093+Rajveer-CSE-27@users.noreply.github.com>
2024-08-03 12:29:51 +0530

    Create introduction.cpp

---
+++ Range Queries/introduction.cpp
@@ -0,0 +1,50 @@
+// || जय जय जय बजरंग बली ||
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+/**
+
+-> What are range queries?
+	Let you are given a sequence of values :: a1, a2, a3, a4, ......
+	These values could be or array, tree, graph, etc sequence.
+	You are given queries of type : [l,r]
+	And for each query you need to apply a fun(al, al+1, al+2, ..., ar) and generate the result.
+	These are called range queries.
+	Worst time to do it is :: O(n*q) but we will improve this in this topic.
+	
+-> Types of queries :: 
+		1. Offline queries.
+				When you are given all input queries at once and told to generate output at end.
+		2. Online queries.
+				When you are given a query and you need to generate immediate result of current query before moving to next query.
+				OR
+				When the input of current query depends on output of previous query or queries.
+				
+				
+		NOTE:: Mo's algorithm is very famous for handling offline queries.
+
+-> Types of updates :: 
+		1. Point update :: 
+				If in a query you are told to update a particular value then it is point update.
+		2. Range update ::
+		 		If in a query you are told to update a particular range of values [l,r] then it is range update.
+		 		
+-> Some popular concepts of dealing with range queries ::
+		
+		Prefix arrays
+		Segment
+		Fenwick
+		Mo's algo
+		Square root decomposition.
+		Sparse table
+		Disjoint set data structures.
+		etc.
+
+**/
+
+int main() {
+	
+	return 0;
+}
