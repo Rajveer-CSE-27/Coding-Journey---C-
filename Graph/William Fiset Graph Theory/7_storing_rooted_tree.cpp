31811787b9b425729e5a8f35b956b1a6ed1aae90
Rajveersinh Gohil <140502093+Rajveer-CSE-27@users.noreply.github.com>
2024-08-04 12:14:32 +0530

    Create 7_storing_rooted_tree.cpp

---
+++ Graph/William Fiset Graph Theory/7_storing_rooted_tree.cpp
@@ -0,0 +1,25 @@
+// || जय जय जय बजरंग बली ||
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+/**
+https://www.youtube.com/watch?v=1XC3p2zBK34&list=PLDV1Zeh2NRsDGO4--qE8yH72HFL1Km93P&index=7
+
+Best way to store a rooted binary tree is using arrays.
+This technique also works for n-ary trees.
+
+But in binary tree ::
+	In an array, if the node is at index i then left child :: 2*i + 1 and right child at :: 2*i + 2;
+	Also accessing the parent is very fast :: O(1) . For a node at index :: i the parent is at index :: floor((i-1)/2).
+**/
+
+
+
+int main() {
+	
+	
+	
+	return 0;
+}
