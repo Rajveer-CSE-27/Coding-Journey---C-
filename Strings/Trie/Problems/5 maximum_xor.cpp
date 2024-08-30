6c2ca351f549f06726a0a78c490ed1b653c907c7
Rajveersinh Gohil <140502093+Rajveer-CSE-27@users.noreply.github.com>
2024-08-30 10:56:03 +0530

    Create 5 maximum_xor.cpp

---
+++ Strings/Trie/Problems/5 maximum_xor.cpp
@@ -0,0 +1,93 @@
+// || जय जय जय बजरंग बली ||
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+/**
+https://www.naukri.com/code360/problems/maximum-xor_973113?leftPanelTabValue=SUBMISSION
+
+Binary trie
+**/
+
+#include<bitset>
+
+struct TrieNode {
+    TrieNode* arr[2] = {NULL};
+
+    bool contains(int i) {
+        return (arr[i] != NULL);
+    }
+    void add(int i) {
+        arr[i] = new TrieNode();
+    }
+    TrieNode* getNextTrieNode(int i) {
+        return arr[i];
+    }
+};
+
+class Trie {
+    TrieNode* root;
+
+public:
+
+    Trie() {
+        root = new TrieNode();
+    }
+    void insert(int n) {
+        bitset<32> b(n);
+
+        TrieNode* temp = root;
+        for(int i=31; i>=0; i--) {
+            int index;
+            (b.test(i)) ? index = 1 : index = 0;
+
+            if(temp->contains(index) == false) {
+                temp->add(index);
+            }
+            temp = temp->getNextTrieNode(index);
+        }
+    }
+
+    int findMaxXor(int x) {
+        bitset<32> b(x);
+        TrieNode* temp = root;
+
+        int ans = 0;
+        for(int i=31; i>=0; i--) {
+            int index;
+            (b.test(i)) ? index = 0 : index = 1;
+
+            if(temp->contains(index) == true) {
+                ans += (1<<i);
+                temp = temp->getNextTrieNode(index);
+            } else {
+                temp = temp->getNextTrieNode(index^1);
+            }
+        }
+
+        return ans;
+    }
+};
+
+int maxXOR(int n, int m, vector<int> &arr1, vector<int> &arr2) 
+{
+    // Write your code here.   
+    Trie trie;
+    for(int i=0; i<n; i++) {
+        trie.insert(arr1[i]);
+    }
+    int ans = 0;
+    for(int i=0; i<m; i++) {
+        int temp = trie.findMaxXor(arr2[i]);
+        ans = max(ans, temp);
+    }
+    return ans;
+}
+
+int main() {
+    
+    
+    
+    
+    return 0;
+}
