16e77d3e941d3885820a892347358a9722da301e
Rajveersinh Gohil <140502093+Rajveer-CSE-27@users.noreply.github.com>
2024-10-16 17:07:15 +0530

    Create intro.cpp

---
+++ Regular Expression/intro.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#include <regex>
+using namespace std;
+
+
+/**
+https://www.tutorialspoint.com/cpp_standard_library/regex.htm
+https://cplusplus.com/reference/regex/
+
+Regular expressions are a standardized way to express patterns to be matched against sequences of characters (string in some cases).
+#include <regex>
+
+Target Sequence (subject) :: This is a sequence of characters on which we perform things i.e a subject. Generally specified by 2 iterators. (start, end)
+                   Some functions of regex class also accept string instead of sequence of characters.
+
+Regular Expression (pattern) ::  The pattern which is searched for in the target sequence. Generally a string.
+
+Matches array :: Some regec operations allow to retrieve information about matches. This info is stored in special arrays called Matches array.
+                 Ex. cmatch or smatch.
+                 
+
+**/
+
+
+int main() {
+  
+  
+  return 0;
+}
