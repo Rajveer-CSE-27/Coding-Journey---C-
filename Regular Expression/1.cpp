8f983104e89f4bedbb7e8d665ee762914c005e10
Rajveersinh Gohil <140502093+Rajveer-CSE-27@users.noreply.github.com>
2024-10-16 19:08:45 +0530

    Create 1.cpp

---
+++ Regular Expression/1.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#include <regex>
+using namespace std;
+
+
+/**
+https://www.youtube.com/watch?v=9K4N6MO_R1Y&t=7s
+**/
+
+void printAllMatches(string s, regex rgx) {
+  
+  sregex_iterator it(s.begin(), s.end(), rgx); // iterator of valid matches of rgx in s
+  // currently it is pointing to first location of match
+  
+  
+  sregex_iterator last; // end location of above defined matches
+  
+  while(it != last) {
+    
+    smatch curr = *it; // smatch is an instantiation of the match_results
+    cout << curr.str() << '\n';
+    
+    it++;
+  }
+  
+  // other functions of smatch are str(), position(), and length()
+  // curr.length() will give length of current matching
+  // curr.position() will give 0 based position of current matching in original string
+  
+}
+
+int main() {
+  
+ 
+  string s = "The ape was at apex"; // this is the string on which we will be performing things.
+  regex rgx("(ape[^ ]?)"); // this is the pattern. Humesha "()" ke andar hi daalna
+  
+  // () is necessary in c++
+  // ape[^ ] : we want all strings that start with ape and then any character except a space.
+  // [^ ] this means not a space
+  // ? That is after ape if there is nothing then ape would also be considered as a match
+  
+  printAllMatches(s, rgx);
+  
+  return 0;
+}
