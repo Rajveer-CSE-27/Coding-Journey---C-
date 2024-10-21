50b2831ba3ded9735879406b72eb1ada9b93ca44
Rajveersinh Gohil <140502093+Rajveer-CSE-27@users.noreply.github.com>
2024-10-21 16:56:35 +0530

    Create phonepe_cashback.cpp

---
+++ Google/PhonePe/phonepe_cashback.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+const int mod = 1e9 + 7;
+
+/**
+https://www.geeksforgeeks.org/maximum-sum-subarray-removing-one-element/
+Similar to this problem
+**/
+
+
+
+  
+int main() {
+  
+  int n;
+  cin >> n;
+  vector<int> vc(n);
+  
+  for(int i=0; i<n; i++) {
+    cin >> vc[i]; 
+  }
+  
+  vector<int> pref(n), suff(n);
+  // pref[i] = max subarray sum till i
+  // suff[i] = max subarray sum from i till last
+  
+  int sm = 0;
+  
+  for(int i=0; i<n; i++) {
+    if(sm + vc[i] < 0) {
+      sm = 0;
+    } else {
+      
+      sm += vc[i];
+    }
+    pref[i] = sm;
+  }
+  
+  sm = 0;
+  for(int i=n-1; i>=0; i--) {
+    
+    if(sm + vc[i] < 0) {
+      sm = 0;
+    } else {
+      
+      sm += vc[i];
+    }
+    suff[i] = sm;
+  }
+  
+  
+  int ans = INT_MAX;
+  
+  for(int i=0; i<n; i++) {
+    
+    if(vc[i] < 0) {
+      // replacing the ith element
+      int left = 0, right = 0;
+      if(i-1 >= 0) {
+        left = pref[i-1];
+      } 
+      if(i+1 < n) {
+        right = suff[i+1];
+      }
+      
+      int temp = -vc[i];
+      temp += left + right;
+      
+      ans = min(ans, temp); // as we need to get maximum phonepe profit hence minimum subarray sm
+    }
+    
+  }
+  
+  
+  if(ans == INT_MAX) {
+    // all elements are positive in vc  
+    for(int i=0; i<n; i++) {
+      ans += vc[i];
+    }
+  }
+  
+  cout << ans;
+  
+  return 0;
+  
+  
+}
